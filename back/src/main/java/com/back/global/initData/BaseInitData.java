ccb9067d5ef053dbc82778b944583d02f77cf10b
puppywimy <eric250@naver.com>
2026-06-24 13:43:09 +0900

    16강: devInitDataApplicationRunner에 Profile("dev")를 추가

--- back/src/main/java/com/back/global/initData/BaseInitData.java
+++ back/src/main/java/com/back/global/initData/BaseInitData.java
@@ -11,6 +11,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
@@ -30,6 +31,7 @@ public class BaseInitData {
         };
     }
 
+    @Profile("dev")
     @Bean
     ApplicationRunner devInitDataApplicationRunner() {
         return args -> {
