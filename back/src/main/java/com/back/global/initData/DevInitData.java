177dad6943e43e8b1f522f99656763f05220b331
puppywimy <eric250@naver.com>
2026-06-25 21:07:23 +0900

    18강: Ut.cmd.run 메서드 만들어서 도입

--- back/src/main/java/com/back/global/initData/DevInitData.java
+++ back/src/main/java/com/back/global/initData/DevInitData.java
@@ -1,12 +1,10 @@
 package com.back.global.initData;
 
+import com.back.standard.util.Ut;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 @Profile("dev")
 @Configuration
@@ -14,37 +12,13 @@ public class DevInitData {
     @Bean
     ApplicationRunner devInitDataApplicationRunner() {
         return args -> {
-            boolean isWindows = System
-                    .getProperty("os.name")
-                    .toLowerCase()
-                    .contains("win");
-
-            ProcessBuilder builder = new ProcessBuilder(
-                    "npx" + (isWindows ? ".cmd" : ""),
+            Ut.cmd.run(
+                    "npx{{DOT_CMD}}",
                     "--yes",
                     "--package", "typescript@v5",
                     "--package", "openapi-typescript",
                     "openapi-typescript", "http://localhost:8080/v3/api-docs/apiV1",
-                    "-o", "../front/src/lib/backend/apiV1/schema.d.ts"
-            );
-
-            // 에러 스트림도 출력 스트림과 함께 병합
-            builder.redirectErrorStream(true);
-
-            // 프로세스 시작
-            Process process = builder.start();
-
-            // 결과 출력
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line); // 결과 한 줄씩 출력
-                }
-            }
-
-            // 종료 코드 확인
-            int exitCode = process.waitFor();
-            System.out.println("종료 코드: " + exitCode);
+                    "-o", "../front/src/lib/backend/apiV1/schema.d.ts");
         };
     }
 }
