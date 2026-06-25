10daea2c72ea8931fac0c25f0727bba53eeb5216
puppywimy <eric250@naver.com>
2026-06-25 21:05:55 +0900

    17강: 개발환경에서 자바 소스코드 변경시 자동으로 스키마 재생성 되도록

--- back/src/main/java/com/back/global/initData/DevInitData.java
+++ back/src/main/java/com/back/global/initData/DevInitData.java
@@ -1,19 +1,50 @@
 package com.back.global.initData;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 @Profile("dev")
 @Configuration
-@Slf4j
 public class DevInitData {
     @Bean
     ApplicationRunner devInitDataApplicationRunner() {
         return args -> {
-            log.debug("개발 환경에서 초기 데이터가 설정되었습니다!");
+            boolean isWindows = System
+                    .getProperty("os.name")
+                    .toLowerCase()
+                    .contains("win");
+
+            ProcessBuilder builder = new ProcessBuilder(
+                    "npx" + (isWindows ? ".cmd" : ""),
+                    "--yes",
+                    "--package", "typescript@v5",
+                    "--package", "openapi-typescript",
+                    "openapi-typescript", "http://localhost:8080/v3/api-docs/apiV1",
+                    "-o", "../front/src/lib/backend/apiV1/schema.d.ts"
+            );
+
+            // 에러 스트림도 출력 스트림과 함께 병합
+            builder.redirectErrorStream(true);
+
+            // 프로세스 시작
+            Process process = builder.start();
+
+            // 결과 출력
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line); // 결과 한 줄씩 출력
+                }
+            }
+
+            // 종료 코드 확인
+            int exitCode = process.waitFor();
+            System.out.println("종료 코드: " + exitCode);
         };
     }
 }
