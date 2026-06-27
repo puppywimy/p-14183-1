33b1825eb15a2fdf389af98c465bd3861f0b211b
puppywimy <eric250@naver.com>
2026-06-27 12:37:10 +0900

    31강: 예외 핸들러 중 하나에 @ResponseStatus(BAD_REQUEST)를 붙여서 스웨거에서 실패 케이스 추가, 스키마에도 해당내용이 자동으로 반영됨

--- back/src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ back/src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Comparator;
@@ -106,11 +107,16 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ServiceException.class)
-    public RsData<Void> handle(ServiceException ex, HttpServletResponse response) {
+    @ResponseStatus(BAD_REQUEST)
+    public ResponseEntity<RsData<Void>> handle(ServiceException ex, HttpServletResponse response) {
         RsData<Void> rsData = ex.getRsData();
 
-        response.setStatus(rsData.statusCode());
-
-        return rsData;
+        return new ResponseEntity<>(
+                rsData,
+                ResponseEntity
+                        .status(rsData.statusCode())
+                        .build()
+                        .getStatusCode()
+        );
     }
 }
